e1f131021a7787cbae7943f50fed15221e499ea3
LaBierre <vincenzo_seggio@yahoo.de>
2017-08-22 16:15:10 +0200

    updated with functionable Parceable

--- app/src/main/java/com/example/standard/popularmoviesstageone/MovieDetailActivity.java
+++ app/src/main/java/com/example/standard/popularmoviesstageone/MovieDetailActivity.java
@@ -1,13 +1,11 @@
 package com.example.standard.popularmoviesstageone;
 
 import android.content.Intent;
-import android.support.v4.app.NavUtils;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
